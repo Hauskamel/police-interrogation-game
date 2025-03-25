9bf1e0cbd50f48424148abd07d614350815a8e0f
krojerLukas <lukas.krojer@gmx.de>
2025-03-25 12:11:05 +0100

    remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,7 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { Road } from "./components/Road"
 import { Car } from "./components/Car"
 
-import { useEffect } from "react";
-
 import './App.css'
-
-
-function CameraHelper () {
-    const camera = new PerspectiveCamera(60, 10, 10, 20);
-
-    return (
-        <cameraHelper args={[camera]} />
-    )
-
-}
-
-
-
-
-function CameraLogger () {
-    const { camera } = useThree();
-
-    useEffect(() => {
-        window.alert(camera.position)
-    }, [camera]);
-
-    return null;
-}
-
 
 function App() {
 
@@ -39,9 +13,6 @@ function App() {
             <ambientLight />
             <directionalLight position={ [5,5,5] } />
             <OrbitControls />
-
-            <CameraHelper />
-            <CameraLogger />
 
             <Road />
             <Car />
