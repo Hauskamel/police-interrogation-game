147f0e5d4ad80d5decf0a3cd48f314692ee64c78
krojerLukas <lukas.krojer@gmx.de>
2025-03-27 17:45:46 +0100

    remove redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,6 @@ import { Car } from "./components/Car"
 import './App.css'
 import {Streetbay} from "./components/Streetbay.jsx";
 
-
-//
-// function CameraHelper () {
-//     const camera = new PerspectiveCamera(60, 1, 1, 1)
-//     console.log(camera)
-//     return <cameraHelper args={[camera]} />;
-// }
-
-
-
 function App() {
     return (
         <Canvas camera={ {position: [2, 2, 2], fov: 90} } >
@@ -27,10 +17,6 @@ function App() {
             <Road />
             <Car />
             <Streetbay />
-
-            {/* Camera */}
-            {/*<CameraHelper />*/}
-
 
         </Canvas>
     )
