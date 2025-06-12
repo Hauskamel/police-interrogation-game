639ad7ddca9d55ae0cfb771fb7b60fcaf512a5ff
Lukas Krojer <Lukas_Krojer@reyrey.com>
2025-06-12 15:06:42 +0200

    add drivers first- and last name to textbox when at policeman

--- src/components/textboxes/CarOccupantsInformationTextbox.jsx
+++ src/components/textboxes/CarOccupantsInformationTextbox.jsx
@@ -4,18 +4,22 @@ export function CarOccupantsInformationTextbox({selectedCar, stoppedCar}) {
     
     if (stoppedCar?.id !== selectedCar?.id) return;
 
-
     return (
         <>
             <BaseTextbox title={"Fahrzeuginsassen"} isCloseable={false}>
                 <div className="flex">
                     {!stoppedCar ?
-                        // TODO: Stand jetzt wird beim anklicken eines 2. Fahrzeugs noch das Bild des vorgängers angezeigt
-                        <img src={`/images/driver/driver-unknown.jpg`} className="w-12 h-12 rounded-full"/>
+                        <>
+                            <img src={`/images/driver/driver-unknown.jpg`} className="w-16 h-16 rounded-full"/>
+                            <p className="text-gray-500 text-xs">unknown</p>
+                        </>
                         :
-                        <img src={`/images/driver/${stoppedCar.profileInformation.driverImage}`} className="w-12 h-12 rounded-full"/>
+                        <>
+                            <img src={`/images/driver/${stoppedCar.profileInformation.driverImage}`} className="w-16 h-16 rounded-full"/>
+                            <p className="text-gray-500 text-xs">{stoppedCar.profileInformation.firstName} {stoppedCar.profileInformation.lastName}</p>
+                        </>
+                        
                     }
-                    <p className="text-gray-500 text-xs">unknown</p>
                 </div>
             </BaseTextbox>
         </>
