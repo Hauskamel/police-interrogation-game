a42fd6c10ba90184b6367b8dc9cb01211c703dfa
lukas krojer <lukas.krojer@web.de>
2025-07-13 19:20:16 +0200

    remove console log

--- src/components/textboxes/CarAndDriverProfileTextbox.jsx
+++ src/components/textboxes/CarAndDriverProfileTextbox.jsx
@@ -9,9 +9,6 @@ export const CarAndDriverProfileTextbox = ({
     // check if no car has been stopped OR user clicked on a car that is not the stopped car
     if (!stoppedCar || stoppedCar.id !== selectedCar?.id) return;
 
-    console.log(selectedCar);
-    
-
     return (
         <>
             <BaseTextbox title={"Fahrer- & Fahrzeugprofil"} margin="bottom-60" isCloseable={false}>
