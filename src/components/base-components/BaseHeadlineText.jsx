165fe975178a2a82fe8fda6664c02395c30597a3
lukas krojer <lukas.krojer@web.de>
2025-07-25 17:18:41 +0200

    fix bug where BaseHeadlineText with different useCase but same Id where both removed from compareArray instead of only the deselected one

--- src/components/base-components/BaseHeadlineText.jsx
+++ src/components/base-components/BaseHeadlineText.jsx
@@ -44,7 +44,11 @@ export const BaseHeadlineText = ({useCase, headline, data, id}) => {
         }
 
         // finds the clicked object by searchin the useCase f.e. 'driversLicence' and the id
-        const obj = compareArray.find(item => item.useCase === useCase && item.id === id)        
+        const obj = compareArray.find(item => item.useCase === useCase && item.id === id)
+
+        console.log(obj);
+        
+
         if (obj) removeInformationFromCompareArray(obj.useCase, obj.id)
         setIsSelected(prev => !prev) // switches between true and false
         return // return so not set again to compareArray
--- src/store.js
+++ src/store.js
@@ -103,7 +103,7 @@ export const useDiscrepandancyCompareStore = create(set => ({
     // FIXME: das funktioniert noch nicht
     removeInformationFromCompareArray: (useCase, id) =>
         set((state) => ({
-            compareArray: state.compareArray.filter((information) => information.id !== id)
+            compareArray: state.compareArray.filter((information) => information.useCase !== useCase)
         })
     ),
     clearCompareArray: () =>
