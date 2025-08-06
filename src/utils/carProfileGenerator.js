7caafc3c4051de6c41c141202ef5925e48f6a06f
lukas krojer <lukas.krojer@web.de>
2025-08-06 21:54:57 +0200

    wip - working on 'applyRandomManipulations' function

--- src/utils/carProfileGenerator.js
+++ src/utils/carProfileGenerator.js
@@ -2,7 +2,6 @@
 // NOTE: ----- for testing purposes its only for the car -----
 
 
-import { percent } from 'framer-motion';
 import { carProfiles } from './carProfiles';
 import { faker } from "@faker-js/faker";
 
@@ -10,24 +9,59 @@ import { faker } from "@faker-js/faker";
 
 const carBrands = Object.keys(carProfiles)
 function getRandomCarBrand(exclude = null) {
-    const carBrandFilter = exclude ? carBrands.filter(brand => brand !== exclude) : carBrands;
-    const carBrand = faker.helpers.arrayElement(carBrandFilter)
-    return carBrand
+    const carBrand = exclude ? carBrands.filter(brand => brand !== exclude) : carBrands;
+    return faker.helpers.arrayElement(carBrand)
 }
 
-const createFakeProfile = Math.floor(Math.random() * 100) < 80; // 50% chance of creating a fake profile
+// chance generator for random choices
+const randomChance = (percent) => {
+    return Math.random() < percent / 100
+}
+
+const manipulations = [
+    profile => ({
+        ...profile,
+        brandName: getRandomCarBrand(profile.brandName)
+    }),
+    profile => ({
+        ...profile,
+        brandModel: carProfile.models[Math.floor(Math.random() * carProfile.models.length)]
+    }),
+    profile => ({
+        ...profile,
+        carRegistrationNumber: faker.vehicle.vrm()
+    }),
+    profile => ({
+        ...profile,
+        plateNumber: "AC - " + carRegistrationNumber.slice(2).replace(/^(.{2})/, '$1 ')
+    })
+]
+
+
+const applyRandomManipulations =  (profile) => {
+    const chancesOfManipulation = [100, 50, 10, 5];
+
+    do {
+        const chance = randomChance(chancesOfManipulation[0])
+        chancesOfManipulation.shift() // removes first element of array
+    } while (chance <= chancesOfManipulation[0])
+
+}
+
 
 
 // this is the actual/original/legitimate profile of the car 
-const generateRealProfile = () => {
+export const generateCarProfile = () => {
     // ########## CAR INFORMATION ##########
     // #####################################
     // car brand
+    
     const brandName = getRandomCarBrand();
     const carProfile = carProfiles[brandName]
-
+    
+    
     // car model
-    const brandModel =  carProfile.models[Math.floor(Math.random() * carProfile.models.length)]
+    const brandModel = carProfile.models[Math.floor(Math.random() * carProfile.models.length)]
 
     // car registration
     const carRegistrationNumber = faker.vehicle.vrm();
@@ -35,43 +69,22 @@ const generateRealProfile = () => {
     // car plate
     const plateNumber = "AC - " + carRegistrationNumber.slice(2).replace(/^(.{2})/, '$1 ')
 
-    return {
+    const realProfile = {
         brandName,
         brandModel,
         plateNumber,
         carRegistrationNumber,
         plateNumber
     }
-}
-const realProfile = generateRealProfile();
 
 
-const manipulateProfile = (profile) => {
-    const chancesOfError = [100, 50, 10, 5];
+    // Generate chance of a manipulated (fake) profile being generated
+    let fakeProfile = null;
+    if (randomChance(50)) {
+        fakeProfile = applyRandomManipulations(realProfile)
+    }
 
-    const manipulations = []
-
-    do {
-        const percentage = Math.floor(Math.random() * 100);
-        console.log(percentage);
-
-        if (percentage <= chancesOfError[0]) {
-            chancesOfError.shift();
-
-            // TODO: Hier dann Logik für Manipulation einfügen
-
-        } else {
-            break;
-        }
-    } while (chancesOfError.length)
-    
-}
-
-
-export const identity = () => {
-    return realProfile;
-}
-
-if (createFakeProfile) {
-    manipulateProfile(realProfile);
+    return fakeProfile ?
+        { realProfile, fakeProfile } :
+        { realProfile }
 }
