32ca1079f6c160802a7e00a10520eff71c70170b
Lukas Krojer <lukas.krojer@gmx.de>
2026-01-29 12:51:15 +0100

    wip: working on npc criminal type and spawn system

---
+++ src/data/crimeTypes.js
@@ -0,0 +1,11 @@
+export const crimeTypes = {
+    smuggler: {
+        inventory: ["AK-47", "Glock 18", "Mariuhana", "MDMA"]
+    },
+    murderer: {
+        inventory: ["Human body of a woman", "Human body of a man", "Weapon with blood on it"]
+    },
+    cybercriminal: {
+        inventory: ["Laptop but it is locked", "Laptop and it seems unlocked"]
+    }
+}
--- src/hooks/useVehicleEntityGenerator.jsx
+++ src/hooks/useVehicleEntityGenerator.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
-import { generateUUID, randInt } from "three/src/math/MathUtils.js";
+import { randInt } from "three/src/math/MathUtils.js";
+
+import { crimeTypes } from "../data/crimeTypes.js";
 
 import { useCarStore, useNpcStore } from "../store.js";
 import { basicEntityProfile } from "../utils/generators/entityProfileGenerators/basicEntityProfileGenerator.js";
@@ -14,32 +16,81 @@ export function useVehicleEntityGenerator (direction, lane) {
         const intervalId = setInterval(() => {
 
 
-            const spawnCarOfWantedList = Math.random() < 0.5;
+            // TODO: PART FOR SPAWNING ENTITIS NEEDS TO BE REWRITTEN
+            // ######################################
+            // ######################################
+            // NEW VERSION:
+            // ######################################
+            // ######################################
+            const spawnCriminal = Math.random() < 0.5; // is npc either a 'normal' civilian or a criminal/smuggler
             let newEntity;
 
-            
-            if (spawnCarOfWantedList && criminalDatabase.length) {
-                // criminalDatabase is a parameter of this function.
-                // the passed value is a reference to the criminalDatabase in the storage.js
-                const criminal = criminalDatabase[Math.floor(Math.random() * criminalDatabase.length)];
-                newEntity = {...criminal, id: generateUUID()}
-
-                
-            } else {
+            if (!spawnCriminal) {
                 newEntity = basicEntityProfile();
+            } else {
+                // spawn a criminal
+                const spawnSmuggler = Math.random() < 0.5; // decide wether criminal is a smuggler;
+                
+                const keys = Object.keys(crimeTypes);
+                const crimeType = keys[Math.floor(Math.random() * keys.length)]
+                console.log(crimeType);
+
+                // TODO: hier funktion einbauen
+                switch (crimeType) {
+                    case "smuggler":
+                        console.log("spawn a smuggler.");
+                        break;
+                    case "murderer":
+                        console.log("spawn a murderer.");
+                        break;
+                    case "cybercriminal":
+                        console.log("spawn a murderer.");
+                        break;
+                }
+                
+
+                // code for criminal;
+
             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // ######################################
+            // ######################################
+            // OLD VERSION:
+            // ######################################
+            // ######################################
+            // const spawnCarOfWantedList = Math.random() < 0.5;
+            // let newEntity;
+
+            
+            // if (spawnCarOfWantedList && criminalDatabase.length) {
+            //     // criminalDatabase is a parameter of this function.
+            //     // the passed value is a reference to the criminalDatabase in the storage.js
+            //     const criminal = criminalDatabase[Math.floor(Math.random() * criminalDatabase.length)];
+            //     newEntity = {...criminal, id: generateUUID()}
+
+
+            // } else {
+            //     newEntity = basicEntityProfile();
+            // }
 
 
 
             newEntity = {...newEntity, spawn: {direction, lane}};
 
             addCar(newEntity);
-
-
-
-
-
-
 
         }, respawnTime, criminalDatabase);
         return () => clearInterval(intervalId);
